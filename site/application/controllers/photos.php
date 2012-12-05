c6b905f2f0bac94fdf091178a422f7434d698bdb
Mark Blackwell <mblackwell8@gmail.com>
2012-12-05 16:26:56 +1100

    fixed uploader

--- site/application/controllers/photos.php
+++ site/application/controllers/photos.php
@@ -77,7 +77,7 @@ class Photos extends CI_Controller {
     }
 
     function _put_data($data) {
-        $success = file_put_contents('application/models/db_all.json', json_encode($data));
+        $success = file_put_contents('application/models/db_all.json', $this->_json_format(json_encode($data)));
         return $success;
     }
 
@@ -212,8 +212,86 @@ class Photos extends CI_Controller {
 
     }
     public function delete_photo() {
-
     }
+
+    // lifted from http://php.net/manual/en/function.json-encode.php#80339
+    function _json_format($json)
+    {
+        $tab = "  ";
+        $new_json = "";
+        $indent_level = 0;
+        $in_string = false;
+
+        $json_obj = json_decode($json);
+
+        if($json_obj === false)
+            return false;
+
+        $json = json_encode($json_obj);
+        $len = strlen($json);
+
+        for($c = 0; $c < $len; $c++)
+        {
+            $char = $json[$c];
+            switch($char)
+            {
+                case '{':
+                case '[':
+                    if(!$in_string)
+                    {
+                        $new_json .= $char . "\n" . str_repeat($tab, $indent_level+1);
+                        $indent_level++;
+                    }
+                    else
+                    {
+                        $new_json .= $char;
+                    }
+                    break;
+                case '}':
+                case ']':
+                    if(!$in_string)
+                    {
+                        $indent_level--;
+                        $new_json .= "\n" . str_repeat($tab, $indent_level) . $char;
+                    }
+                    else
+                    {
+                        $new_json .= $char;
+                    }
+                    break;
+                case ',':
+                    if(!$in_string)
+                    {
+                        $new_json .= ",\n" . str_repeat($tab, $indent_level);
+                    }
+                    else
+                    {
+                        $new_json .= $char;
+                    }
+                    break;
+                case ':':
+                    if(!$in_string)
+                    {
+                        $new_json .= ": ";
+                    }
+                    else
+                    {
+                        $new_json .= $char;
+                    }
+                    break;
+                case '"':
+                    if($c > 0 && $json[$c-1] != '\\')
+                    {
+                        $in_string = !$in_string;
+                    }
+                default:
+                    $new_json .= $char;
+                    break;                   
+            }
+        }
+
+        return $new_json;
+    } 
 }
 
 /* End of file welcome.php */
